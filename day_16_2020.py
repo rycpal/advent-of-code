bfe7cf99b8860951e995fb26d3e10848bbc21ed2
ryc <ryc.pal.pe@gmail.com>
2021-03-02 21:59:18 -0500

    day 16 2020 part 2

--- day_16_2020.py
+++ day_16_2020.py
@@ -27,11 +27,11 @@ def inputdata():
 def scanning_error(fields, nearby_tickets):
     ranges = list()
     error = 0
-    for record in fields.values():
-        ranges.append(record[:2])
-        ranges.append(record[2:])
-    for record in nearby_tickets:
-        for item in record:
+    for field in fields.values():
+        ranges.append(field[:2])
+        ranges.append(field[2:])
+    for ticket in nearby_tickets:
+        for item in ticket:
             for minor, mayor in ranges:
                 if minor <= item <= mayor:
                     break
@@ -39,7 +39,59 @@ def scanning_error(fields, nearby_tickets):
                 error += item
     return error
 
+def searching(fields, our_ticket, nearby_tickets):
+    # remove error tickets
+    ranges = list()
+    for field in fields.values():
+        ranges.append(field[:2])
+        ranges.append(field[2:])
+    error = set()
+    for ti, ticket in enumerate(nearby_tickets):
+        for item in ticket:
+            for minor, mayor in ranges:
+                if minor <= item <= mayor:
+                    break
+            else:
+                error.add(ti)
+    error = list(error)
+    error.sort(reverse=True)
+    for ti in error:
+        nearby_tickets.pop(ti)
+    # add our ticket
+    nearby_tickets.append(our_ticket)
+    # search candidates
+    columns = [ list() for co in our_ticket ]
+    for ro in range(len(nearby_tickets)):
+        for co in range(len(columns)):
+            columns[co].append(nearby_tickets[ro][co])
+    candidates = { key:list() for key in fields.keys() }
+    for key, ranges in fields.items():
+        for co in range(len(columns)):
+            for value in columns[co]:
+                if not(ranges[0] <= value <= ranges[1] or ranges[2] <= value <= ranges[3]):
+                    break
+            else:
+                candidates[key].append(co)
+    # reduce candidates
+    while candidates != dict():
+        column = None
+        for key in candidates.keys():
+            if len(candidates[key]) == 1:
+                column = candidates[key].pop()
+                fields[key].append(column)
+                del candidates[key]
+                break
+        for candidate in candidates.values():
+            candidate.remove(column)
+    # departure fields
+    value = 1
+    for name in fields.keys():
+        if name.find('departure') >= 0:
+            value *= our_ticket[fields[name][4]]
+    return value
+
 if __name__ == '__main__':
     print('\n16: Ticket Translation')
     fields, our_ticket, nearby_tickets = inputdata()
     print('\nerror =',scanning_error(fields, nearby_tickets))
+    print("\ndeparture fields =",searching(fields, our_ticket, nearby_tickets))
